e457cf38714772804bffd335e7ff995febaad7d9
05duchuy <05duchuy@email.com>
2026-09-07 14:07:59 +0700

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+        allowedHosts: [
+          'webgenpic.onrender.com'
+        ],
         port: 3000,
         host: '0.0.0.0',
       },
